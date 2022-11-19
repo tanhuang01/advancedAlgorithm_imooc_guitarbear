3e69897f006fea44d3a24de43a4c965fd6159a34
congguangzi <congspark@gmail.com>
2022-11-19 15:30:00 +0800

    delete several .java file which can not comprise the project.

--- src/ad_dp/palin/Palindrome.java
+++ src/ad_dp/palin/Palindrome.java
@@ -33,14 +33,14 @@ public class Palindrome {
             the number of palindromic line is dp[i][j].
          */
         dp[1][n] = 1;
-        for (int i = 1; i < n; i++) {
-            for (int o = i + 1; o >= 1; o--) {
-                for (int u = n - i; u <= n; u++) {
-                    if (a[o][i + 2 - o] == a[u][n * 2 - i - u]) {
-                        dp[o][u] = dp[o][u] + dp[o - 1][u] + dp[o][u + 1] + dp[o - 1][u + 1];
-                        dp[o][u] %= MOD;
+        for (int k = 1; k < n; k++) {
+            for (int i = k + 1; i >= 1; i--) {
+                for (int j = n - k; j <= n; j++) {
+                    if (a[i][k + 2 - i] == a[j][n * 2 - k - j]) {
+                        dp[i][j] = dp[i][j] + dp[i - 1][j] + dp[i][j + 1] + dp[i - 1][j + 1];
+                        dp[i][j] %= MOD;
                     } else {
-                        dp[o][u] = 0;
+                        dp[i][j] = 0;
                     }
                 }
             }
